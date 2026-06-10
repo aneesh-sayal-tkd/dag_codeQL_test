10195ff807953b0de8fb02c3719b1c0f42033366
aneesh-sayal-tkd <aneesh.sayal@takeda.com>
2026-06-10 19:41:20 +0530

    Update pascal_triangle.py

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -7,14 +7,14 @@ def print_pascal_triangle(n):
     """
     for i in range(n):
         # Print leading spaces for triangle formatting
-        print(' ' * (n - i - 1), end='')
+        print(' ' * (n - i - 1), end=''
         
         # Calculate and print values for current row
         value = 1
         for j in range(i + 1):
             print(value, end=' ')
             # Calculate next value using binomial coefficient formula
-            # C(i, j+1) = C(i, j) * (i - j) / (j + 1)
+            # C(i, j+1) = C(i, j) * (i - j) / (j + 1
             value = value * (i - j) // (j + 1)
         
         print()  # New line after each row
